f91495fb75c82fe50377a001b79cf7a147cf5989
agamsatria <agamsatria25@gmail.com>
2024-05-04 00:18:25 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,32 +1,28 @@
 <?php
 
-	//Get variable from env
-	$db_user = getenv('DB_USERNAME', true) ?: getenv('DB_USERNAME');
-	$db_pass = getenv('DB_PASSWORD', true) ?: getenv('DB_PASSWORD');
-    $db_name = getenv('DB_DATABASE', true) ?: getenv('DB_DATABASE');
+$db_user = getenv('DB_USERNAME', true)?: getenv('DB_USERNAME');
+$db_pass = getenv('DB_PASSWORD', true)?: getenv('DB_PASSWORD');
+$db_name = getenv('DB_DATABASE', true)?: getenv('DB_DATABASE');
 
+define('APP_URL', '');
+$_app_stage = 'Live';
 
-    define('APP_URL', '');
-    $_app_stage = 'Live';
+$db_host = 'localhost';
+$db_user = $db_user;
+$db_password = $db_pass;
+$db_name = $db_name;
 
-    // Database PHPNuxBill
-    $db_host        = 'localhost';
-    $db_user        = $db_user;
-    $db_password    = $db_pass;
-    $db_name        = $db_name;
+$radius_host = 'localhost';
+$radius_user = $db_user;
+$radius_pass = $db_pass;
+$radius_name = $db_name;
 
-    // Database Radius
-    $radius_host        = 'localhost';
-    $radius_user        = $db_user;
-    $radius_pass        = $db_pass;
-    $radius_name        = $db_name;
-
-    if($_app_stage!='Live'){
-        error_reporting(E_ERROR);
-        ini_set('display_errors', 1);
-        ini_set('display_startup_errors', 1);
-    }else{
-        error_reporting(E_ERROR);
-        ini_set('display_errors', 0);
-        ini_set('display_startup_errors', 0);
-    }
+if ($_app_stage!= 'Live') {
+    error_reporting(E_ERROR);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+} else {
+    error_reporting(E_ERROR);
+    ini_set('display_errors', 0);
+    ini_set('display_startup_errors', 0);
+}
